cabf8d7a7f2d841acc53a4118cff898ac691675e
Matt Steele <orphum@gmail.com>
2010-12-10 11:42:05 -0600

    Adding some .gitignores, and making sure people don't cheat at roulette.

--- tools/roulette.erl
+++ tools/roulette.erl
@@ -2,6 +2,8 @@
 
 -export([choose/1]).
 
+choose([]) -> {error, "Not so fast!" };
+	      
 choose(People) ->
     RandomItem = random:uniform(length(People)),
     io:fwrite("The unlucky individual: ~w~n", [lists:nth(RandomItem, People)]),
