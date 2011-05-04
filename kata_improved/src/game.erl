ff31208bb48893b5cf161261937705f25fd056f7
swicklund <swicklund@pobox.com>
2011-05-03 21:28:53 -0500

    Here is the improved code

--- kata_improved/src/game.erl
+++ kata_improved/src/game.erl
@@ -18,50 +18,29 @@
 
 new() -> [].
 
-roll(Pins, GameData) -> GameData ++ [Pins].
+roll(Pins, [{Roll1} | MoreData]) when Roll1 < 10  -> [{Roll1, Pins}|MoreData];
+roll(Pins, GameData) -> [{Pins}|GameData].
 
-score([Roll|[SecondRoll|[ThirdRoll|[]]]]) 
-  when Roll == 10 ->
-	io:format("score([~p | [~p | [~p | GameData]]])~n",
-			  [Roll, SecondRoll, ThirdRoll]),
-	10 + SecondRoll + ThirdRoll;
-score([Roll|[SecondRoll|[ThirdRoll|GameData]]]) 
-  when Roll == 10 ->
-	io:format("score([~p | [~p | [~p | GameData]]])~n",
-			  [Roll, SecondRoll, ThirdRoll]),
-	10 + SecondRoll + ThirdRoll 
-		+ score([SecondRoll|[ThirdRoll|GameData]]);
+score(GameData) -> scoreGame(1, lists:reverse(GameData)).
+	
 
-score([FirstRoll|[SecondRoll | [NextRoll| []]]]) 
-  when FirstRoll + SecondRoll == 10 -> 
-	io:format("score([~p | [~p | [~p | GameData]]])~n",
-			  [FirstRoll, SecondRoll, NextRoll]),
-	10 + NextRoll;  %%SPARE
-  
-score([FirstRoll|[SecondRoll | [NextRoll| GameData]]]) 
-  when FirstRoll + SecondRoll == 10 -> 
-	io:format("score([~p | [~p | [~p | GameData]]])~n",
-			  [FirstRoll, SecondRoll, NextRoll]),
-	10 + NextRoll + score([NextRoll|GameData]);  %%SPARE
-
-
-score([FirstRoll|[SecondRoll | [NextRoll| GameData]]]) ->
-    io:format("score([~p | [~p | [~p | GameData]]])~n",
-			  [FirstRoll, SecondRoll, NextRoll]),
-	 FirstRoll + SecondRoll + score([NextRoll|GameData]);
-
-score([FirstRoll|[SecondRoll|[]]]) ->
-	io:format("score([~p | [~p | []]])~n",
-			  [FirstRoll, SecondRoll]),
-	FirstRoll+SecondRoll;
-score([Roll|[]]) -> 
-	io:format("score([~p ])~n",
-			  [Roll]),
-	Roll;
-score([]) ->
-	io:format("score([])~n",[]),
-	0.
 %%
 %% Local Functions
 %%
+scoreGame(FrameNumber, _) when FrameNumber > 10 -> 0;  %%Additional frames only added to 10th frame
+scoreGame(FrameNumber, [Frame|GameData]) ->
+	case Frame of 
+		{10} -> 10 + getRoll(1, GameData) + getRoll(2, GameData) + scoreGame(FrameNumber + 1, GameData);
+		{Roll1, Roll2} when Roll1 + Roll2 == 10 -> 10 + getRoll(1, GameData) + scoreGame(FrameNumber + 1, GameData);
+		{Roll1, Roll2} -> Roll1 + Roll2 + scoreGame(FrameNumber + 1, GameData)
+	end.
 
+
+getRoll(_Position, []) -> 0;
+getRoll(Position, [Frame| GameData]) -> 
+	if 
+		Position > tuple_size(Frame) -> getRoll(Position - tuple_size(Frame), GameData);
+	    true -> element(Position, Frame)
+	end.
+	
+	
